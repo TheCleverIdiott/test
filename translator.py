f51d99c3147ced28ee7eefcbea7f3e950c2e0a34
Aritra Ghosh ⚡ <aritrag1905@gmail.com>
2024-02-02 21:10:20 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -3,23 +3,27 @@ from PIL import Image  # for opening image files
 from datetime import date  # provides date & time functions
 from gtts import gTTS, lang  # for text speech
 from googletrans import Translator  # provides translation functions
+from PyPDF2 import PdfReader
 
 # setting app's title, icon & layout
 st.set_page_config(page_title="Advo Translate", page_icon="🎯")
 
 
 def get_key(val):
-    """function to find the key of the given value in the dict object
-
-    Args:
-        val (str): value to find key
-
-    Returns:
-        key(str): key for the given value
-    """
     for key, value in lang.tts_langs().items():
         if val == value:
             return key
+        
+
+def extract_text_from_pdf(file_path):
+    # Open the PDF file using PyPDF2
+    with open(file_path, "rb") as f:
+        reader = PdfReader(f)
+        page = reader.pages[0]
+        text = page.extract_text()
+    return text
+
+choice = st.sidebar.selectbox("Select your choice", ["Translate Text", "Translate Document"])
 
 
 def main():
@@ -29,61 +33,77 @@ def main():
     # gets gtts supported languages as dict
     langs = lang.tts_langs()
 
+
+    if choice == "Translate Document":
     # display current date & header
-    st.header("Advo Translator")
-    st.write(f"Date : {date.today()}")
+        st.header("Advo Translator")
+        st.write(f"Date : {date.today()}")
 
-    input_text = st.text_input("Enter the text")  # gets text to translate
-    lang_choice = st.selectbox(
-        "Language to translate: ", list(langs.values())
-    )  # shows the supported languages list as selectbox options
+        input_text = st.text_input("Enter the text")  # gets text to translate
+        lang_choice = st.selectbox(
+            "Language to translate: ", list(langs.values())
+        )  # shows the supported languages list as selectbox options
 
-    if st.button("Translate"):
-        if input_text == "":
-            # if the user input is empty
-            st.write("Please Enter text to translate")
+        if st.button("Translate"):
+            if input_text == "":
+                # if the user input is empty
+                st.write("Please Enter text to translate")
 
-        else:
-            detect_expander = st.expander("Detected Language")
-            with detect_expander:
-                detect = trans.detect([input_text])[
-                    0
-                ]  # detect the user given text language
-                detect_text = f"Detected Language : {langs[detect.lang]}"
-                st.success(detect_text)  # displays the detected language
+            else:
+                detect_expander = st.expander("Detected Language")
+                with detect_expander:
+                    detect = trans.detect([input_text])[
+                        0
+                    ]  # detect the user given text language
+                    detect_text = f"Detected Language : {langs[detect.lang]}"
+                    st.success(detect_text)  # displays the detected language
 
-                # convert the detected text to audio file
-                detect_audio = gTTS(text=input_text, lang=detect.lang, slow=False)
-                detect_audio.save("user_detect.mp3")
-                audio_file = open("user_detect.mp3", "rb")
-                audio_bytes = audio_file.read()
-                st.audio(audio_bytes, format="audio/ogg", start_time=0)
+                    # convert the detected text to audio file
+                    detect_audio = gTTS(text=input_text, lang=detect.lang, slow=False)
+                    detect_audio.save("user_detect.mp3")
+                    audio_file = open("user_detect.mp3", "rb")
+                    audio_bytes = audio_file.read()
+                    st.audio(audio_bytes, format="audio/ogg", start_time=0)
 
-            trans_expander = st.expander("Translated Text")
-            with trans_expander:
-                translation = trans.translate(
-                    input_text, dest=get_key(lang_choice)
-                )  # translates user given text to target language
-                translation_text = f"Translated Text : {translation.text}"
-                st.success(translation_text)  # displays the translated text
+                trans_expander = st.expander("Translated Text")
+                with trans_expander:
+                    translation = trans.translate(
+                        input_text, dest=get_key(lang_choice)
+                    )  # translates user given text to target language
+                    translation_text = f"Translated Text : {translation.text}"
+                    st.success(translation_text)  # displays the translated text
 
-                # convert the translated text to audio file
-                translated_audio = gTTS(
-                    text=translation.text, lang=get_key(lang_choice), slow=False
-                )
-                translated_audio.save("user_trans.mp3")
-                audio_file = open("user_trans.mp3", "rb")
-                audio_bytes = audio_file.read()
-                st.audio(audio_bytes, format="audio/ogg", start_time=0)
-
-                # download button to download translated audio file
-                with open("user_trans.mp3", "rb") as file:
-                    st.download_button(
-                        label="Download",
-                        data=file,
-                        file_name="trans.mp3",
-                        mime="audio/ogg",
+                    # convert the translated text to audio file
+                    translated_audio = gTTS(
+                        text=translation.text, lang=get_key(lang_choice), slow=False
                     )
+                    translated_audio.save("user_trans.mp3")
+                    audio_file = open("user_trans.mp3", "rb")
+                    audio_bytes = audio_file.read()
+                    st.audio(audio_bytes, format="audio/ogg", start_time=0)
+
+                    # download button to download translated audio file
+                    with open("user_trans.mp3", "rb") as file:
+                        st.download_button(
+                            label="Download",
+                            data=file,
+                            file_name="trans.mp3",
+                            mime="audio/ogg",
+                        )
+
+    elif choice == "Translate Document":
+        st.header("DOCS Translator")
+        st.write(f"Date : {date.today()}")
+
+        file = st.file_uploader("Upload a PDF", type=["pdf"])
+        if file:
+            text = extract_text_from_pdf(file)
+            st.write(text)
+            lang_choice = st.selectbox(
+                "Language to translate: ", list(langs.values())
+            )
+
+                
 
 
 if __name__ == "__main__":
